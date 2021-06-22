98fb8af0195f75ca5280cc6bf45b81d9ba1fd6e6
Patrick ZHUANG <patrickzhuang@hotmail.fr>
2021-06-22 20:11:58 +0200

    disconnect when token expired

--- src/app/common-tipschef/interceptor/token.interceptor.ts
+++ src/app/common-tipschef/interceptor/token.interceptor.ts
@@ -8,17 +8,20 @@ import {
 import { Observable } from 'rxjs';
 import {tap} from 'rxjs/operators';
 import {Router} from '@angular/router';
+import {AuthService} from '../../shared/services/auth/auth.service';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,
+              private authService: AuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(request).pipe(tap(event => {
       return event;
     }, error => {
       if (error.status === 401 && error.error.detail === 'Token expired'){
+        this.authService.disconnect();
         this.router.navigate(['/log-in'], {queryParams: {returnUrl: this.router.routerState.snapshot.url}});
       }
     }));
